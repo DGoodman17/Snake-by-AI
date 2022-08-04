924e7bfa047855808e719951e6b7eaf5e45241ff
DGoodman17 <Goodma.Daniel17@svvsd.org>
2022-08-04 09:48:32 -0600

    Main PyGame code

---
+++ script.py
@@ -0,0 +1,18 @@
+import pygame
+from time import sleep
+import random
+
+snake_speed = 15
+
+# Windows size
+window_x = 720
+window_y = 480
+
+# Define colors
+black = pygame.color(0, 0, 0, 0)
+white = pygame.color(255, 255, 255)
+red = pygame.color(255, 0, 0)
+green  = pygame.color(0, 255, 0)
+blue = pygame.color(0, 0, 255)
+
+pygame.init()
