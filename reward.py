e7be3dc67824f2ee05e3a118e312117bfefb0dc7
Daniel Goodman <Goodma.Daniel17@svvsd.org>
2023-04-07 09:28:57 -0600

    Fixed small minor error on reward.py stating that pygame must be initialized, started the numpy array setting for the rise and decline of the AI's reward if the AI does well or not

--- reward.py
+++ reward.py
@@ -1,8 +1,24 @@
 # Main import block
 import numpy as np
+from state import GameState
+from logging import raiseExceptions
+import pygame
 
+# Init visual screen for the game
+pygame.init()
+
+# Main var block
+state = GameState()
 
 class AiReward():
   def __init__(reward_value, reward_value_real):
     np.array(reward_value)
     np.array(reward_value_real)
+
+    if state.snake_position[0] == state.apple_position[0] and state.snake_position[1] == state.apple_position[1]:
+        reward_value[0] = 10
+        reward_value_real[0] = 10
+        raise Exception("AI Reward has gone up")
+    elif state.snake_position[0] == state.apple_position[0] and state.snake_position[1]!= state.apple_position[1]:
+      reward_value[0] = -10
+      raise Exception("AI Reward has gone down")
