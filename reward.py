925a46d260cd1889da2a3eb90206ac484dfe896a
Daniel Goodman <Goodma.Daniel17@svvsd.org>
2023-04-07 09:18:39 -0600

    Fixed local numpy issues and started reward function in reward.py, continuing to make a reward function using numpy as np and array

--- reward.py
+++ reward.py
@@ -1,5 +1,8 @@
+# Main import block
+import numpy as np
 
 
 class AiReward():
   def __init__(reward_value, reward_value_real):
-    pass
+    np.array(reward_value)
+    np.array(reward_value_real)
