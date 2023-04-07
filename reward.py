75fffbabe57eea84f63a30e44bd4d3848999425c
Daniel Goodman <Goodma.Daniel17@svvsd.org>
2023-04-07 09:58:31 -0600

    Fix on reward

--- reward.py
+++ reward.py
@@ -9,7 +9,13 @@ pygame.init()
 
 # Main var block
 state = GameState()
+highscore = 0
+score = state.score
+initial_reward = 0
+highest_reward = 0
+current_reward = 0
 
+#Main code block
 class AiReward():
   def __init__(reward_value, reward_value_real):
     np.array(reward_value)
@@ -22,3 +28,15 @@ class AiReward():
     elif state.snake_position[0] == state.apple_position[0] and state.snake_position[1]!= state.apple_position[1]:
       reward_value[0] = -10
       raise Exception("AI Reward has gone down")
+if highscore < score:
+    raise Exception("New highscore")
+    highscore = score
+    console.log("Hightscore was set to: " + str(highscore))
+def check_reward_score(initial_reward, highest_reward):
+    if initial_reward > highest_reward:
+        raise Exception("New high on reward")
+        highest_reward = current_reward
+        console.log("Newest highscore on reward: " + str(highest_reward))
+    elif initial_reward < highest_reward:
+        raise Exception("No new high on reward")
+        console.log("No new highscore on reward.")
