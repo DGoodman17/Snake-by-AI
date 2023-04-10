46aee1fd74513c9f7bd57fd148ffa015528f910a
Daniel Goodman <Goodma.Daniel17@svvsd.org>
2023-04-09 20:38:59 -0600

    Started to improve AI and starting training

--- ai.py
+++ ai.py
@@ -1,13 +1,24 @@
-import script, state, reward
-import gym
+#Main import block
+#All Sorce code and article: https://github.com/vedantgoswami/SnakeGameAI/blob/main/model.py
+# https://www.geeksforgeeks.org/ai-driven-snake-game-using-deep-q-learning/
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
 
-class custom_game(gym.Env):
-    def __init__(self, grid_size=12):
-        ...
-        self.action_space = ... # Possible actions
-        self.observation_space =... # Possible observations
-    def _get_obs(self):
-        """Calulates the observations (input) of current state"""
-        ...
-        return self.observation_space
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
     
+    def save(self, file_name='model_name.pth'):
+        model_folder_path = 'Path'
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
